eb4098b83c565bf1c405744da8f3b79e17352845
Christian Hackl <hackl.chris@googlemail.com>
2018-09-17 12:28:00 +0200

    fixed IOS Mobile Nav - scroll/click

--- Resources/Public/JavaScript/MenuMain.js
+++ Resources/Public/JavaScript/MenuMain.js
@@ -28,7 +28,8 @@
             logo = $("#logo"),
             logoCp = logo.clone(),
             menuLanguage = $("#menu-language"),
-            header = $("#header");
+            header = $("#header"),
+            onscroll = false;
 
         $(window).on("resize.pushbar", function() {
             moveMenu(btnMobile, menuPushbar, menuMain, logoCp, menuLanguage, header);
@@ -52,48 +53,57 @@
         };
 
         $(document).on("click.clickhandler", function() {
-            clickHandler();
+            if(onscroll === false) {
+                clickHandler();
+            }
+        });
+
+        $("#menu-pushbar").on("scroll", function() {
+            onscroll = true;
         });
 
         $(document).on("touchend.menuDocTouch", "#menu-main a", function(event) {
-            event.preventDefault();
-            event.stopImmediatePropagation();
+            if(onscroll === false) {
+                console.log("touchend");
+                event.preventDefault();
+                event.stopImmediatePropagation();
 
-            $("#menu-main a").unbind("click");
-            $(document).unbind("click");
+                $("#menu-main a").unbind("click");
+                $(document).unbind("click");
 
-            var el = $(this).parent("li"),
-                childUl = el.children('ul'),
-                link = el.children("a").attr("href"),
-                target = el.children("a").attr("target");
+                var el = $(this).parent("li"),
+                    childUl = el.children('ul'),
+                    link = el.children("a").attr("href"),
+                    target = el.children("a").attr("target");
 
-            if(childUl.length > 0) {
-                if(el.hasClass("open")) {
-                    console.log("close");
-                    el.removeClass("open").children("ul").slideUp();
-                    // openLink(link, target);
+                if(childUl.length > 0) {
+                    if(el.hasClass("open")) {
+                        console.log("close");
+                        el.removeClass("open").children("ul").slideUp();
+                        // openLink(link, target);
+                    } else {
+                        console.log("open");
+                        $("#header nav .open").removeClass("open").children("ul").slideUp();
+                        el.addClass("open").children("ul").slideDown();
+                    }
+
+                    if(!btnMobile.is(":visible") && !$("html").hasClass("touch-menu1")) {
+                        $("html").addClass("touch-menu1");
+                        menuMain.find("li").each(function(i, li) {
+                            var $this = $(li),
+                                childUl = $this.children('ul'),
+                                menuLiA = $this.children("a"),
+                                linkHelper = menuLiA.attr("href"),
+                                targetHelper = menuLiA.attr("target"),
+                                linkNameHelper = menuLiA.text();
+                            if(childUl.length > 0) {
+                                childUl.prepend("<li class='touch-menu-helper'><a href='"+linkHelper+"' target='"+targetHelper+"'>Öffne "+linkNameHelper+"</a></li>");
+                            }
+                        });
+                    }
                 } else {
-                    console.log("open");
-                    $("#header nav .open").removeClass("open").children("ul").slideUp();
-                    el.addClass("open").children("ul").slideDown();
+                    openLink(link, target);
                 }
-
-                if(!btnMobile.is(":visible") && !$("html").hasClass("touch-menu1")) {
-                    $("html").addClass("touch-menu1");
-                    menuMain.find("li").each(function(i, li) {
-                        var $this = $(li),
-                            childUl = $this.children('ul'),
-                            menuLiA = $this.children("a"),
-                            linkHelper = menuLiA.attr("href"),
-                            targetHelper = menuLiA.attr("target"),
-                            linkNameHelper = menuLiA.text();
-                        if(childUl.length > 0) {
-                            childUl.prepend("<li class='touch-menu-helper'><a href='"+linkHelper+"' target='"+targetHelper+"'>Öffne "+linkNameHelper+"</a></li>");
-                        }
-                    });
-                }
-            } else {
-                openLink(link, target);
             }
         });
     });
