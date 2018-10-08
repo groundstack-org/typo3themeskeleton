595501bd179388a55e89f9fad166770736a7c470
ioCron <info@iocron.com>
2018-10-08 20:57:44 +0200

    Cookie Consent Config Outsourced

---
+++ Resources/Public/JavaScript/Libs/cookieconsent.custom.js
@@ -0,0 +1,19 @@
+window.addEventListener("load", function(){
+    window.cookieconsent.initialise({
+        "palette": {
+            "popup": {
+              "background": "#27697e",
+              "text": "#ffffff"
+            },
+            "button": {
+              "background": "#ffffff",
+              "text": "#27697e"
+            }
+        },
+        "content": {
+            "message": "Cookies erleichtern die Bereitstellung unserer Dienste. Mit der Nutzung unserer Dienste erklären Sie sich damit einverstanden, dass wir Cookies verwenden.",
+            "dismiss": "Einverstanden!",
+            "link": "Mehr Informationen"
+        }
+    });
+});
